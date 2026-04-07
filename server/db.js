1b9b80bc7e878d28c4ce24905c206f4234ad934c
Muhtasim Ishmam <muhtasim175@gmail.com>
2026-04-07 15:24:52 -0600

    Added debugging to see if db password is being recognised or not by server/.env

--- server/db.js
+++ server/db.js
@@ -4,12 +4,23 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
 });
+console.log({
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+});
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD exists:", !!process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 module.exports = pool;
